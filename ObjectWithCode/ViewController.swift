17d7330285ad3b76fe6e96bd5f5dc313d50a9da9
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-25 12:15:32 +0300

    last commit

--- ObjectWithCode/ViewController.swift
+++ ObjectWithCode/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let myLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        
+        myLabel.text = "My Test Label"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.1, y: height * 0.3, width:
+        width * 0.8, height: 50)
+        view.addSubview(myLabel)
+        
+        
+        
+        let myButton = UIButton()
+        myButton.frame = CGRect(x: width*0.2, y: height*0.5, width: width*0.6, height: 50)
+        myButton.setTitle("My Button", for: UIControlState.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        view.addSubview(myButton)
+        
+        
+        myButton.addTarget(self, action: #selector(ViewController.myAction) , for: UIControlEvents.touchUpInside)
+        
+    }
+    
+    @objc func myAction() {
+        
+        myLabel.text = "You Clicked"
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
